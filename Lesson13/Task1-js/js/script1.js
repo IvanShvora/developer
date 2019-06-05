35117107d232b0e63e3490cfa3ad8c0e1fd4cf44
Ivan Shvora <mirajumbra@gmail.com>
2019-06-05 16:07:39 +0300

    com

---
+++ Lesson13/Task1-js/js/script1.js
@@ -0,0 +1,118 @@
+    //  var title="imput masages";
+    // var result=prompt(title,"");
+    // console.log (result);
+
+
+
+ 
+
+
+    // var i = 1;
+    // do {
+    // console.log(i*i);
+    // i++;
+    // } while (i < 10);
+    
+  
+
+// for(var i=1; i<=9; i++)
+  // console.log(i*i)
+
+
+//  for(var i=0; i<=5; i++)
+//  console.log(i)
+
+
+//  var a=result;
+//  if (a>0){console.log(1);}
+//  else if (a<0){console.log(-1);}
+//  else {a=0}{console.log(0);}
+
+
+//  if (result==null){
+//      console.log("Hello World");
+//  }
+//  else if(result=="31"){
+//      console.log(result);
+//      }
+//  else {
+//     console.log(false);
+//  }
+
+    // var a=2+2;
+    // switch (a) {
+    //     case 3:
+    //     console.log ('Малувато');
+    //     break;
+    //     case 4:
+    //     console.log ('В точку');
+    //     break;
+    //     case 5:
+    //     console.log ('Перебор');
+    //     break;
+    //     default:
+    //     console.log ('Takyx znan');
+    //   }
+
+
+// for (i=0; i<10; i++){
+//     if (i===3){continue;}
+//     console.log(i);
+// }
+
+// var arr=['a',1,3,];
+// arr [0]=4;
+// console.log(arr)
+
+// var computer1={
+//     modell:'hp notebook',
+//     cpu: 'intel'
+// }
+// console.log (computer1);
+
+// function showName() {
+//     console.log ('My name')
+// }
+// showName();
+
+
+// var masage='Pruvit';
+// function showMessage(){
+// message='Hello';
+// }
+// console.log (masage);
+
+// function checkingNumber(a,b){
+// if (a>b){
+// return a+'a>b';
+// }
+// else if(b>a){
+//     return b+'b>a';
+// }
+// else{a==b}{
+// return a+"a=b";
+// }
+// }
+// console.log (checkingNumber(9,5));
+
+// var Date=new Date();
+// alert (Date);
+// console.log ();
+
+
+  //  var a=1;
+  //  switch (a) {
+  //      case 1:
+  //      console.log ('a');
+  //      break;
+  //      case 2:
+  //      console.log ('b');
+  //      break;
+  //      case 3:
+  //      console.log ('c');
+  //      break;
+  //      default:
+  //      console.log ('z');
+  //    }
+
+  
